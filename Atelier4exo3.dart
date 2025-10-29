d4e3e981245d78f27e86e6f58ec571a0ac0dfbf7
Hanane <Hananehenouna23@gmail.com>
2025-10-28 21:30:44 -0400

    Exercice 3 - Question 4: Creation d'un constructeur simplifie Compte

---
+++ Atelier4exo3.dart
@@ -0,0 +1,39 @@
+
+
+  class Compte {
+  // Propriété privée pour le solde
+  double _solde;
+
+  // Propriété finale pour le numéro du compte
+  final String numeroCompte;
+
+  // Constructeur principal simplifié
+  Compte(this.numeroCompte, [this._solde = 0.0]);
+
+  // Méthode pour consulter le solde
+  double get solde {
+    return _solde;
+  }
+
+  // Méthode pour déposer de l'argent
+  void deposer(double montant) {
+    if (montant > 0) {
+      _solde += montant;
+      print("Vous avez déposé $montant. Votre nouveau solde est : $_solde");
+    } else {
+      print("Merci d'effectuer un depot valide.");
+    }
+  }}
+
+
+
+void main() {
+  // Création d'un compte avec solde initial par défaut
+  Compte monCompte = Compte("333",0.0);
+
+  print("Votre Numéro de compte est: ${monCompte.numeroCompte}");
+  print("Solde initial: ${monCompte.solde}");
+//Exemple de dépôt 
+ monCompte.deposer(500);
+ print("Nouveau solde après dépôt : ${monCompte.solde}"); 
+}
