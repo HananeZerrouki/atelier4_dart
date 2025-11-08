9f861c2a6ba865cee418feef829130f2a2202f43
Hanane <Hananehenouna23@gmail.com>
2025-11-08 14:36:03 -0500

    Ajout d'une méthode concrète void afficherMessage() qui affiche Calcul de l'aire...

---
+++ Atelier4exo4.dart
@@ -0,0 +1,42 @@
+class Media{
+  final String titre;
+  Media(this.titre);
+  void afficherType(){
+    print("Ceci est un média générique.");
+  }
+}
+class Livre extends Media{
+  final String auteur;
+  Livre(super.titre, this.auteur);
+  @override
+   void afficherType(){
+    print("Ceci est un Livre : $titre par $auteur.");
+  }
+}
+class Film extends Media{
+  final int dureeMinutes;
+  Film(super.titre, this.dureeMinutes);
+  @override
+   void afficherType(){
+    print("Ceci est un Film : $titre de duree $dureeMinutes.");
+  }
+}
+void main(){
+  List <Media> Catalogue = [];
+  Livre livre1 = Livre ('Comment eduquer votre enfant','hanane');
+  Livre livre2 = Livre ('Art culinaire','karim');
+  Film film1= Film('Titanic',120);
+  Film film2= Film('Final Destination',120);
+  Catalogue.add(livre1);
+  Catalogue.add(livre2);
+  Catalogue.add(film1);
+  Catalogue.add(film2);
+  
+  
+
+  for (var media in Catalogue){
+    media.afficherType();
+  }
+
+
+}
---
+++ Atelier4exo5.dart
@@ -0,0 +1,55 @@
+import 'dart:math';
+
+abstract class Forme {
+  double calculerAire();//methode abstraite
+  void afficherMessage(){
+    //methode concrete
+    print("Calcule de l'aire...");
+  }
+}
+
+class Cercle extends Forme {
+  final double rayon;
+  Cercle(this.rayon);
+  @override 
+  double calculerAire() {
+    double aire= rayon*rayon*pi;
+    return aire;
+  }}
+  class Rectangle extends Forme {
+  final double longeur;
+  final double largeur;
+  Rectangle(this.longeur,this.largeur);
+
+  @override 
+  double calculerAire() {
+    double aire= longeur*largeur;
+    return aire;
+  }}
+
+void main() {
+  var C1 = Cercle(5);
+  
+  C1.afficherMessage();
+  double aire1= C1.calculerAire();
+  print("Aire du cercle C1 est : $aire1");
+  var C2 = Cercle(2);
+  C2.afficherMessage();
+  double aire2= C2.calculerAire();
+  print("Aire du cercle C2 est : $aire2");
+  
+  
+  var r1 = Rectangle(5,2);
+  
+  r1.afficherMessage();
+  r1.calculerAire();
+  double aire3= r1.calculerAire();
+  print("Aire du rectangle r1 est : $aire3");
+  
+  var r2 = Rectangle(6,3);
+r2.afficherMessage();
+  r2.calculerAire();
+  double aire4= r2.calculerAire();
+  print("Aire du rectangle r2 est : $aire4");
+  
+}
