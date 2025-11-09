c6c03a77f3628247960670a6e64db984a3139605
Hanane <Hananehenouna23@gmail.com>
2025-11-08 20:51:29 -0500

    Exercice8: Question9

--- Atelier4exo8.dart
+++ Atelier4exo8.dart
@@ -19,17 +19,32 @@ static int totalLivres=0;
     print("Le nombre Total de livres crees est : $totalLivres");
   }
   }
-
- 
-
+  class Roman extends Livre{
+    String genre;
+    Roman(super.titre,super.auteur,this.genre,[super._pages=200]);//constructeur de la classe Roman 
+  
+    @override // redefinition de la methode afficherInfo de la classe mere : Livre
+    void afficherInfo(){// la methode qui affiche toutes les informations pour les objets cree de cette classe s'apelle afficherInfo 
+  super.afficherInfo();  //Appel la methode de la classe mere qui permet d'afficher titre,auteur et nombre de pages de chaque livre 
+  print("Ce livre est de genre: $genre");// On affiche aussi l'information genre
+}
+  }
 void main() {
   var l1= Livre("How to be Happy", "Hanane");//le livre l1 va prendre la valeur par defaut 200 pour son nombre de pages 
   var l2= Livre("L'art culinaire kabyle", "Karim",7);
   var l3= Livre("Be intelligent,Be you", "Matine",20);
+  var r1=Roman("Il ya quoi apres la mort ","Wahiba","Philosophie");
+  var r2=Roman("Enjoy your wedding life","Wahiba","Romantic");
+  var r3=Roman("Coupe de monde ","Wahiba","Sport",30);
 
   l1.afficherInfo();
   l2.afficherInfo();
   l3.afficherInfo();
+  r1.afficherInfo();
+  r2.afficherInfo();
+  r3.afficherInfo();
+
+  //Ici on apelle la methode statique qui affiche le nombre total de livre cree
   Livre.afficherTotal();
   
 }
