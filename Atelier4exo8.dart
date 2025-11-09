6492f00f672e9cb1291837adb8cbb73b141852b3
Hanane <Hananehenouna23@gmail.com>
2025-11-08 19:35:17 -0500

    Exercice8: Question7

--- Atelier4exo8.dart
+++ Atelier4exo8.dart
@@ -1,23 +1,35 @@
 class Livre{
   String titre;
   String auteur;
+int _pages;
+static int totalLivres=0;
 
   // constructeur
-  Livre(this.titre, this.auteur);
+  Livre(this.titre, this.auteur,[this._pages=200]){
+    totalLivres++;}
   void afficherInfo(){
-    print("Votre Livre est de titre: $titre, ecrit par l'auteur: $auteur");
+    print("Votre Livre est de titre: $titre, ecrit par l'auteur: $auteur, qui a :$_pages pages");
+  }
+  // Creation d'un getter pages pour lire le nombre de pages 
+  int get pages {
+    return _pages;
+  }
+  //une methode statique qui affiche le nombre total de livres crees
+   static void afficherTotal() {
+    print("Le nombre Total de livres crees est : $totalLivres");
   }
   }
 
  
 
 void main() {
-  var l1= Livre("How to be Happy", "Hanane");
-  var l2= Livre("L'art culinaire kabyle", "Karim");
-  var l3= Livre("Be intelligent,Be you", "Matine");
+  var l1= Livre("How to be Happy", "Hanane");//le livre l1 va prendre la valeur par defaut 200 pour son nombre de pages 
+  var l2= Livre("L'art culinaire kabyle", "Karim",7);
+  var l3= Livre("Be intelligent,Be you", "Matine",20);
 
   l1.afficherInfo();
   l2.afficherInfo();
   l3.afficherInfo();
+  Livre.afficherTotal();
   
 }
